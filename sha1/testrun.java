25c3e92092520ce4ab5ac5fc27bb660d76c1ec85
shevious <shevious@yahoo.com>
2016-11-24 05:49:26 +0000

    hmac_sha1

--- sha1/testrun.java
+++ sha1/testrun.java
@@ -5,18 +5,27 @@ import java.security.NoSuchAlgorithmException;
 
 public class testrun {
     public static void main(String[] args) throws IOException {
-        try {
-	    //byte[] sha1_ad1 = AeSimpleSHA1.SHA1("Buy new mobile phone! Nokia, Sony-Ericsson, Samsung, LG, ...");
-	    //System.out.println("hexhash: "+ AeSimpleSHA1.convertToHex(sha1_ad1));
-	    //System.out.println("hash: "+ sha1_ad1);
+	//byte[] sha1_ad1 = AeSimpleSHA1.SHA1("Buy new mobile phone! Nokia, Sony-Ericsson, Samsung, LG, ...");
+	//System.out.println("hexhash: "+ AeSimpleSHA1.convertToHex(sha1_ad1));
+	//System.out.println("hash: "+ sha1_ad1);
+	byte[] sha1_ad1 = null;
 
-            String key_salt = "django.contrib.auth.models.AbstractBaseUser.get_session_auth_hash";
-            String secret = "wvw18j%u^3ps83e2l#&$!3hrm2!1+us6$2r3h3g91u5udy_+!!";
-	    byte[] sha1_ad1 = AeSimpleSHA1.SHA1(key_salt+secret);
-	    System.out.println("hash: "+ sha1_ad1);
-	    System.out.println("hexhash: "+ AeSimpleSHA1.convertToHex(sha1_ad1));
+	String key_salt = "django.contrib.auth.models.AbstractBaseUser.get_session_auth_hash";
+	String secret = "wvw18j%u^3ps83e2l#&$!3hrm2!1+us6$2r3h3g91u5udy_+!!";
+	try {
+	    sha1_ad1 = AeSimpleSHA1.SHA1(key_salt+secret);
+	} catch (NoSuchAlgorithmException e) {
+	}
+	System.out.println("hash: "+ sha1_ad1);
+	System.out.println("hexhash: "+ AeSimpleSHA1.convertToHex(sha1_ad1));
           
-        } catch (NoSuchAlgorithmException e) {
+        String password = "pbkdf2_sha256$20000$nKuC5T0Aw09Q$kdHmugxSg2tQKHizYue7zFx3d/qFBrMKnIiFGsj5qzE=";
+	String hmac = "";
+	try {
+	    hmac = HmacSha1Signature.calculateRFC2104HMAC(password, sha1_ad1);
+        } catch (Exception e) {
         }
+
+	System.out.println(hmac);
     }
 }
